3072c69b63a2c762f7475c74d2d25e819ad14318
Dzianis Sushchenia <denis.sushchenja@gmail.com>
2023-10-30 04:03:05 +0100

    task5: integrate import API

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, Injector } from '@angular/core';
-import { EMPTY, Observable } from 'rxjs';
-import { ApiService } from '../../core/api.service';
-import { switchMap } from 'rxjs/operators';
+import {Injectable, Injector} from '@angular/core';
+import {EMPTY, Observable} from 'rxjs';
+import {ApiService} from '../../core/api.service';
+import {switchMap} from 'rxjs/operators';
 
 @Injectable()
 export class ManageProductsService extends ApiService {
@@ -18,7 +18,7 @@ export class ManageProductsService extends ApiService {
     }
 
     return this.getPreSignedUrl(file.name).pipe(
-      switchMap((url) =>
+      switchMap(({url}) =>
         this.http.put(url, file, {
           headers: {
             // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -29,10 +29,10 @@ export class ManageProductsService extends ApiService {
     );
   }
 
-  private getPreSignedUrl(fileName: string): Observable<string> {
+  private getPreSignedUrl(fileName: string): Observable<{ url: string }> {
     const url = this.getUrl('import', 'import');
 
-    return this.http.get<string>(url, {
+    return this.http.get<{ url: string }>(url, {
       params: {
         name: fileName,
       },
